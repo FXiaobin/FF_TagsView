240b2e7006d31021554c229ed672601b4bf3474d
liwei <liwei@liweidemacbook-air.local>
2017-03-16 13:14:15 +0800

    Initial Commit

---
+++ TagsView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TagsView
+//
+//  Created by liwei on 17/3/16.
+//  Copyright © 2017年 fanxiaobin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TagsView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TagsView
+//
+//  Created by liwei on 17/3/16.
+//  Copyright © 2017年 fanxiaobin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
